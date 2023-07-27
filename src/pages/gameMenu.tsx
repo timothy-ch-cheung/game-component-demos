908eba1d12721d6c6e7b032d6e99a0c562928edb
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-07-27 23:57:56 +0100

    Add game menu demo

--- src/pages/gameMenu.tsx
+++ src/pages/gameMenu.tsx
@@ -1,5 +1,16 @@
 export function GameMenu() {
-    return <div>
-        <h1>GAME MENU</h1>
+  return (
+    <div>
+      <div style={{ width: "640", height: "480", display: "flex", justifyContent: "center", marginTop: "5%" }}>
+        <iframe
+          title="Game Menu"
+          src="../game-menu.html"
+          width="640"
+          height="480"
+          style={{border: 0 }}
+          scrolling="no"
+        ></iframe>
+      </div>
     </div>
+  );
 }
