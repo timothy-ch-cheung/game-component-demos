51d80d2a0baa5f6b9ec3f4608ddc37e67273ab8f
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-07-28 23:52:49 +0100

    Add map tween demo

--- src/pages/mapTween.tsx
+++ src/pages/mapTween.tsx
@@ -1,5 +1,16 @@
 export function MapTween() {
-    return <div>
-        <h1>MAP TWEEN</h1>
-    </div>
+    return (
+        <div>
+          <div style={{ width: "480", height: "480", display: "flex", justifyContent: "center", marginTop: "5%" }}>
+            <iframe
+              title="Map Tween"
+              src="../map-tween.html"
+              width="480"
+              height="480"
+              style={{border: 0 }}
+              scrolling="no"
+            ></iframe>
+          </div>
+        </div>
+      );
 }
