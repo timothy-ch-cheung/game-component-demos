ce3d1367c347eb98fda439506c4643dfa4e62107
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-07-29 23:53:45 +0100

    Add gifs for home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,12 @@
+import gameMenu from "../assets/game-menu.gif"
+import mapTween from "../assets/map-tween.gif"
+import Stack from '@mui/material/Stack';
+
 export function Home() {
-    return <div>
-        <h1>HOME</h1>
+    return <div style={{display: "flex", justifyContent: "center", margin: "2.5%"}}>
+        <Stack direction="row" spacing={4}>
+            <img src={gameMenu} alt="game-menu" />
+            <img src={mapTween} alt="map-tween" />
+        </Stack>
     </div>
 }
