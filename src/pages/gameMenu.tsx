e52ac8afea2a62527bc985446bebcc7960d9f45c
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-07-31 23:19:41 +0100

    Add loading ring

--- src/pages/gameMenu.tsx
+++ src/pages/gameMenu.tsx
@@ -1,15 +1,33 @@
+import CircularProgress from "@mui/material/CircularProgress";
+
 export function GameMenu() {
   return (
     <div>
-      <div style={{ width: "640", height: "480", display: "flex", justifyContent: "center", marginTop: "5%" }}>
-        <iframe
-          title="Game Menu"
-          src="../game-menu.html"
-          width="640"
-          height="480"
-          style={{border: 0 }}
-          scrolling="no"
-        ></iframe>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
+      >
+        <div
+          style={{ width: "640px", height: "480px", backgroundColor: "grey", zIndex: 1 }}
+        >
+          <div style={{ position: "relative", width: 0, height: 0 }}>
+            <CircularProgress
+              style={{
+                position: "absolute",
+                top: "220px",
+                left: "300px",
+                zIndex: -1,
+              }}
+            />
+          </div>
+          <iframe
+            title="Game Menu"
+            src="../game-menu.html"
+            width="640"
+            height="480"
+            style={{ border: 0 }}
+            scrolling="no"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
--- src/pages/mapTween.tsx
+++ src/pages/mapTween.tsx
@@ -1,16 +1,39 @@
+import CircularProgress from "@mui/material/CircularProgress";
+
 export function MapTween() {
-    return (
-        <div>
-          <div style={{ width: "480", height: "480", display: "flex", justifyContent: "center", marginTop: "5%" }}>
-            <iframe
-              title="Map Tween"
-              src="../map-tween.html"
-              width="480"
-              height="480"
-              style={{border: 0 }}
-              scrolling="no"
-            ></iframe>
+  return (
+    <div>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
+      >
+        <div
+          style={{
+            width: "480px",
+            height: "480px",
+            backgroundColor: "grey",
+            zIndex: 1,
+          }}
+        >
+          <div style={{ position: "relative", width: 0, height: 0 }}>
+            <CircularProgress
+              style={{
+                position: "absolute",
+                top: "220px",
+                left: "220px",
+                zIndex: -1,
+              }}
+            />
           </div>
+          <iframe
+            title="Map Tween"
+            src="../map-tween.html"
+            width="480"
+            height="480"
+            style={{ border: 0 }}
+            scrolling="no"
+          ></iframe>
         </div>
-      );
+      </div>
+    </div>
+  );
 }
