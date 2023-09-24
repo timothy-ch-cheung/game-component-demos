2ebcc434240acc0f184a0be875d2ee4865bb64cc
Tim Cheung <43986931+timothy-ch-cheung@users.noreply.github.com>
2023-09-24 16:19:55 +0100

    Template the HTML used for WASM loading

--- src/pages/blockPlacement.tsx
+++ src/pages/blockPlacement.tsx
@@ -1,39 +1,13 @@
-import CircularProgress from "@mui/material/CircularProgress";
+import GameWindow from "../components/GameWindow";
 
 export function BlockPlacement() {
-    return (
-        <div>
-            <div
-                style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
-            >
-                <div
-                    style={{
-                        width: "720px",
-                        height: "480px",
-                        backgroundColor: "grey",
-                        zIndex: 1,
-                    }}
-                >
-                    <div style={{ position: "relative", width: 0, height: 0 }}>
-                        <CircularProgress
-                            style={{
-                                position: "absolute",
-                                top: "220px",
-                                left: "350px",
-                                zIndex: -1,
-                            }}
-                        />
-                    </div>
-                    <iframe
-                        title="Block Placement"
-                        src="../block-placement.html"
-                        width="720"
-                        height="480"
-                        style={{ border: 0 }}
-                        scrolling="no"
-                    ></iframe>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}>
+      <GameWindow
+        wasmFile="blockPlacement-1-0-0.wasm"
+        width={720}
+        height={480}
+      />
+    </div>
+  );
 }
--- src/pages/gameMenu.tsx
+++ src/pages/gameMenu.tsx
@@ -1,34 +1,9 @@
-import CircularProgress from "@mui/material/CircularProgress";
+import GameWindow from "../components/GameWindow";
 
 export function GameMenu() {
   return (
-    <div>
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}
-      >
-        <div
-          style={{ width: "640px", height: "480px", backgroundColor: "grey", zIndex: 1 }}
-        >
-          <div style={{ position: "relative", width: 0, height: 0 }}>
-            <CircularProgress
-              style={{
-                position: "absolute",
-                top: "220px",
-                left: "300px",
-                zIndex: -1,
-              }}
-            />
-          </div>
-          <iframe
-            title="Game Menu"
-            src="../game-menu.html"
-            width="640"
-            height="480"
-            style={{ border: 0 }}
-            scrolling="no"
-          ></iframe>
-        </div>
-      </div>
+    <div style={{ display: "flex", justifyContent: "center", marginTop: "5%" }}>
+      <GameWindow wasmFile="gameMenu-1-0-0.wasm" width={640} height={480} />
     </div>
   );
 }
